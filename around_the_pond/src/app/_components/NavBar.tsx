9693c2acd7e286d1a574463a3f39e3872c4a5e7a
effilin <effienummer@gmail.com>
2024-11-01 21:13:23 -0400

    Navbar layout updated menu icon added

--- around_the_pond/src/app/_components/NavBar.tsx
+++ around_the_pond/src/app/_components/NavBar.tsx
@@ -3,17 +3,18 @@ import '../styles/globals.css';
 export default function NavBar() {
 
     return (
-            <nav id="navbar" className=" container-fluid text-center p-2 ">
-                
-                    <div className='row '>
-                        <div className='col-1'>
+            <nav> 
+                <div id="navbar" className="container-fluid "> 
+                    <div className='row'>
+                        <div className='col-4'>
+                            <img id="menu" src='menu_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png' alt='menu icon'/>  
+                        </div>
+                        <div className='col-6'>
+                            <h1 className='float-start'>Life Around The Pond</h1>
                             <img id="logo" src='tadpole.png' alt="frog holding onto a stick with reeds" />
                         </div>
-                        <div className='col-10'>
-                            <h1>Life Around The Pond</h1>
-                        </div>
                     </div>
-                
+                </div>
             </nav>
     )
 }
